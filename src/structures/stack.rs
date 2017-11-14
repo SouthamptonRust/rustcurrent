e947a8dac5c44935432202f7956409f335cf551f
Joshua Marron <marronjosh@hotmail.com>
2017-11-14 12:50:56 +0000

    Push works, memory leaking

--- src/structures/stack.rs
+++ src/structures/stack.rs
@@ -1,16 +1,19 @@
 use std::sync::atomic::{AtomicPtr, Ordering};
 use std::ptr;
+use std::fmt::Debug;
 
-pub struct Stack<T: Send + Sync> {
+#[derive(Debug)]
+pub struct Stack<T: Send + Sync + Debug> {
     head: AtomicPtr<Node<T>>
 }
 
-struct Node<T> {
+#[derive(Debug)]
+pub struct Node<T: Debug> {
     data: Option<T>,
-    next: *mut Node<T>
+    next: AtomicPtr<Node<T>>
 }
 
-impl<T: Send + Sync> Stack<T> {
+impl<T: Send + Sync + Debug> Stack<T> {
     pub fn new() -> Stack<T> {
         Stack {
             head: AtomicPtr::default()
@@ -18,26 +21,49 @@ impl<T: Send + Sync> Stack<T> {
     }
 
     pub fn push(&mut self, val: T) {
-        let mut node = Node {
+        let node = Box::into_raw(Box::new(Node {
             data: Some(val),
-            next: ptr::null_mut()
-        };
+            next: AtomicPtr::default()
+        }));
 
         loop {
-            if self.try_push(&mut node) {
+            if self.try_push(node) { // Create mut * to the node
                 break;
             }
         };
     }
 
     fn try_push(&mut self, node: *mut Node<T>) -> bool {
-        let old_head = self.head.load(Ordering::Relaxed);
+        let old_head = self.head.load(Ordering::Acquire);
         unsafe {
-            (*node).next = old_head;
+            (*node).next = AtomicPtr::new(old_head);
         }
         match self.head.compare_exchange_weak(old_head, node, Ordering::AcqRel, Ordering::Acquire) {
             Ok(_) => true,
             Err(_) => false
+        }
+    }
+}
+
+mod tests {
+    use super::Stack;
+    use std::sync::atomic::Ordering;
+
+    #[test]
+    fn test_push_single_threaded() {
+        let mut stack : Stack<u8> = Stack::new();
+
+        stack.push(4u8);
+        println!("{:?}", stack);
+        stack.push(3);
+        println!("{:?}", stack);
+        stack.push(1);
+        println!("{:?}", stack);
+        unsafe {
+            let val = (*stack.head.load(Ordering::Relaxed)).data;
+            assert_eq!(val, Some(1));
+            let next_val = (*(*stack.head.load(Ordering::Relaxed)).next.load(Ordering::Relaxed)).data;
+            assert_eq!(next_val, Some(3));
         }
     }
 }
