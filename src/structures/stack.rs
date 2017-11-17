469c6fb9f1438774a483a09153f229652ca4e703
Joshua Marron <marronjosh@hotmail.com>
2017-11-17 23:41:08 +0000

    Aded Opinfo infrastructure

--- src/structures/stack.rs
+++ src/structures/stack.rs
@@ -24,10 +24,7 @@ impl<'a, T: Send + Sync + Debug> Stack<T> {
 
     pub fn push(&mut self, val: T) {
         // Create a new node on the heap, with a pointer to it
-        let node = Box::into_raw(Box::new(Node {
-            data: Some(val),
-            next: AtomicPtr::default()
-        }));
+        let node = Node::new_as_pointer(val);
 
         loop {
             if self.try_push(node) {
@@ -78,19 +75,30 @@ impl<'a, T: Send + Sync + Debug> Stack<T> {
     }
 }
 
+impl<T: Debug> Node<T> {
+    fn new_as_pointer(val: T) -> *mut Self {
+        Box::into_raw(Box::new(Node {
+            data: Some(val),
+            next: AtomicPtr::default()
+        }))
+    }
+}
+
 struct EliminationLayer<T: Debug> {
-    operations: HashMap<thread::ThreadId, OpInfo<T>>,
+    operations: HashMap<thread::ThreadId, AtomicPtr<OpInfo<T>>>,
         // If we bound the number of threads, and preallocate the HashMap,
         // it should be fine to access concurrently because rehashing will
         // never happen, as guaranteed by the runtime.
-    collisions: Vec<Option<thread::ThreadId>>
+    collisions: Vec<AtomicPtr<Option<thread::ThreadId>>>
 }
 
+#[derive(Clone)]
 struct OpInfo<T: Debug> {
     operation: Option<OpType>,
     node: *mut Node<T>
 }
 
+#[derive(Clone)]
 enum OpType {
     Pop,
     Push
@@ -98,9 +106,85 @@ enum OpType {
 
 impl<T: Debug> EliminationLayer<T> {
     fn new(max_threads: usize, collision_size: usize) -> Self {
+        let mut collisions = Vec::with_capacity(collision_size);
+        for _ in 0..collision_size {
+            collisions.push(AtomicPtr::new(Box::into_raw(Box::new(None))));
+        }
         Self {
             operations: HashMap::with_capacity(max_threads),
-            collisions: vec![None; collision_size]
+            collisions: collisions
+        }
+    }
+
+    fn try_eliminate(&mut self, opinfo: OpInfo<T>) -> Result<T, OpInfo<T>> {
+        let my_info_ptr = Box::into_raw(Box::new(opinfo));
+        self.operations.entry(thread::current().id()).or_insert(AtomicPtr::default()).store(my_info_ptr, Ordering::Acquire);
+        let position = Self::choose_position();
+        let mut them = ptr::null_mut();
+        
+        loop {
+            them = self.collisions[position].load(Ordering::Acquire);
+            let me = Box::into_raw(Box::new(Some(thread::current().id())));
+            if self.collisions[position].compare_exchange_weak(them, me, Ordering::AcqRel, Ordering::Acquire).is_ok() {
+                break;
+            }
+        }
+
+        let mut their_info: Option<&AtomicPtr<OpInfo<T>>> = None;
+        unsafe {
+            if (*them).is_none() {
+                return Err(opinfo);
+            }
+            their_info = self.operations.get(&ptr::read(them).unwrap());
+            if their_info.is_none() {
+                return Err(opinfo);
+            }
+        }
+        
+        unsafe {
+            let their_op = ptr::read(their_info.unwrap().load(Ordering::Acquire));
+            let my_info = ptr::read(my_info_ptr);
+            if my_info.check_complimentary(their_op.operation.as_ref()) {
+
+            }
+        }
+        Err(opinfo)
+    }
+
+    fn choose_position() -> usize {
+        unimplemented!();
+    }
+}
+
+impl<T: Debug> OpInfo<T> {
+    fn new_from_pointer(node: *mut Node<T>, op: OpType) -> Self {
+        OpInfo {
+            operation: Some(op),
+            node
+        }
+    }
+
+    fn new_as_pointer(node: *mut Node<T>, op: OpType) -> *mut Self {
+        Box::into_raw(Box::new({
+            OpInfo {
+                operation: Some(op),
+                node
+            }
+        }))
+    }
+
+    fn new_from_data(data: T, op: OpType) -> Self {
+        OpInfo {
+            operation: Some(op),
+            node: Node::new_as_pointer(data)
+        }
+    }
+
+    fn check_complimentary(&self, op: Option<&OpType>) -> bool {
+        match (self.operation.as_ref(), op) {
+            (Some(&OpType::Push), Some(&OpType::Pop)) => true,
+            (Some(&OpType::Pop), Some(&OpType::Push)) => true,
+            _ => false
         }
     }
 }
