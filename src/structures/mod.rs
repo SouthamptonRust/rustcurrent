3b62b840c97052d884bdd3358f58c45992dd59ac
Joshua Marron <marronjosh@hotmail.com>
2018-03-07 18:08:09 +0000

    Added stubs for hash map

--- src/structures/mod.rs
+++ src/structures/mod.rs
@@ -1,7 +1,9 @@
 pub use self::stack::Stack;
 pub use self::queue::Queue; 
 pub use self::seg_queue::SegQueue;
+pub use self::hash_map::HashMap;
 
 mod stack;
 mod queue;
 mod seg_queue;
+mod hash_map;
