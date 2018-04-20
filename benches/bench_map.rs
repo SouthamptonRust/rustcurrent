98f25fe067550b40a290f706e32c7d5608653aa3
Joshua Marron <marronjosh@hotmail.com>
2018-04-20 01:17:28 +0100

    Benchmarked hashmap against chashmap

--- benches/bench_map.rs
+++ benches/bench_map.rs
@@ -1,9 +1,11 @@
 #[macro_use]
 extern crate criterion;
 extern crate rustcurrent;
+extern crate chashmap;
 
 use criterion::{Bencher, Criterion};
 use rustcurrent::structures::HashMap;
+use chashmap::CHashMap;
 use std::collections;
 
 use std::thread;
@@ -14,7 +16,7 @@ fn bench_equal_focus(num_threads: usize) {
     let map: Arc<HashMap<usize, usize>> = Arc::default();
     let mut wait_vec: Vec<JoinHandle<()>> = Vec::new();
 
-    for _ in 0..num_threads {
+    for _ in 0..num_threads / 2{
         let map_clone = map.clone();
         wait_vec.push(thread::spawn(move || {
             for i in 0..10000 / num_threads {
@@ -22,7 +24,7 @@ fn bench_equal_focus(num_threads: usize) {
             }
         }));
     }
-    for _ in 0..num_threads {
+    for _ in 0..num_threads / 2{
         let map_clone = map.clone();
         wait_vec.push(thread::spawn(move || {
             for i in 0..10000 / num_threads {
@@ -40,7 +42,7 @@ fn bench_equal_focus_lock(num_threads: usize) {
     let map: Arc<Mutex<collections::HashMap<usize, usize>>> = Arc::default();
     let mut wait_vec: Vec<JoinHandle<()>> = Vec::new();
 
-    for _ in 0..num_threads {
+    for _ in 0..num_threads / 2{
         let map_clone = map.clone();
         wait_vec.push(thread::spawn(move || {
             for i in 0..10000 / num_threads {
@@ -49,11 +51,37 @@ fn bench_equal_focus_lock(num_threads: usize) {
         }));
     }
 
-    for _ in 0..num_threads {
+    for _ in 0..num_threads / 2{
         let map_clone = map.clone();
         wait_vec.push(thread::spawn(move || {
             for i in 0..10000 / num_threads {
                 map_clone.lock().unwrap().get(&i);
+            }
+        }));
+    }
+
+    for handle in wait_vec {
+        handle.join().unwrap();
+    }
+}
+
+fn bench_equal_focus_chashmap(num_threads: usize) {
+     let map: Arc<CHashMap<usize, usize>> = Arc::default();
+    let mut wait_vec: Vec<JoinHandle<()>> = Vec::new();
+
+    for _ in 0..num_threads / 2{
+        let map_clone = map.clone();
+        wait_vec.push(thread::spawn(move || {
+            for i in 0..10000 / num_threads {
+                map_clone.insert(i, i);
+            }
+        }));
+    }
+    for _ in 0..num_threads / 2{
+        let map_clone = map.clone();
+        wait_vec.push(thread::spawn(move || {
+            for i in 0..10000 / num_threads {
+                let _ = map_clone.get(&i);
             }
         }));
     }
@@ -144,6 +172,51 @@ fn bench_typical_lock(num_threads: usize) {
             }
             for i in 1000..1200 / num_threads{
                 map_clone.lock().unwrap().remove(&i);
+            }
+        }));
+    }
+
+    for handle in wait_vec {
+        handle.join().unwrap();
+    }
+}
+
+fn bench_typical_chashmap(num_threads: usize) {
+    let map: Arc<CHashMap<usize, usize>> = Arc::default();
+    let mut wait_vec: Vec<JoinHandle<()>> = Vec::new();
+    
+    for _ in 0..num_threads / 2 {
+        let map_clone = map.clone();
+        wait_vec.push(thread::spawn(move || {
+            for i in 0..1000 / num_threads {
+                map_clone.insert(i, i);
+            }
+            for i in 1000..2000 / num_threads{
+                map_clone.get(&i);
+            }
+            for i in 0..7000 / num_threads{
+                map_clone.get(&(i % 1000));
+            }
+            for i in 0..200 / num_threads{
+                map_clone.remove(&i);
+            }
+        }));
+    }
+
+    for _ in 0..num_threads / 2 {
+        let map_clone = map.clone();
+        wait_vec.push(thread::spawn(move || {
+            for i in 1000..2000 / num_threads{
+                map_clone.insert(i, i);
+            }
+            for i in 0..1000 / num_threads{
+                map_clone.get(&i);
+            }
+            for i in 0..7000 / num_threads{
+                map_clone.get(&((i % 1000) + 1000));
+            }
+            for i in 1000..1200 / num_threads{
+                map_clone.remove(&i);
             }
         }));
     }
@@ -255,6 +328,57 @@ fn bench_map_with_updates_lock(num_threads: usize) {
     }
 }
 
+fn bench_with_updates_chashmap(num_threads: usize) {
+    let map: Arc<CHashMap<usize, usize>> = Arc::default();
+    let mut wait_vec: Vec<JoinHandle<()>> = Vec::new();
+    
+    for _ in 0..num_threads / 2 {
+        let map_clone = map.clone();
+        wait_vec.push(thread::spawn(move || {
+            for i in 0..1000 / num_threads{
+                map_clone.insert(i, i);
+            }
+            for i in 1000..2000 / num_threads{
+                map_clone.get(&i);
+            }
+            for i in 0..7000 / num_threads{
+                map_clone.get(&(i % 1000));
+            }
+            for i in 0..200 / num_threads{
+                map_clone.remove(&i);
+            }
+            for i in 200..400 / num_threads{
+                map_clone.insert(i, i + 1);
+            }
+        }));
+    }
+
+    for _ in 0..num_threads / 2 {
+        let map_clone = map.clone();
+        wait_vec.push(thread::spawn(move || {
+            for i in 1000..2000 / num_threads{
+                map_clone.insert(i, i);
+            }
+            for i in 0..1000 / num_threads{
+                map_clone.get(&i);
+            }
+            for i in 0..7000 / num_threads{
+                map_clone.get(&((i % 1000) + 1000));
+            }
+            for i in 1000..1200 / num_threads{
+                map_clone.remove(&i);
+            }
+            for i in 1200..1400 / num_threads{
+                map_clone.insert(i, i + 1);
+            }
+        }));
+    }
+
+    for handle in wait_vec {
+        handle.join().unwrap();
+    }
+}
+
 fn bench_heavy_insert(num_threads: usize) {
     let map: Arc<HashMap<usize, usize>> = Arc::default();
     let mut wait_vec: Vec<JoinHandle<()>> = Vec::new();
@@ -357,6 +481,57 @@ fn bench_heavy_insert_lock(num_threads: usize) {
     }
 }
 
+fn bench_heavy_insert_chashmap(num_threads: usize) {
+    let map: Arc<CHashMap<usize, usize>> = Arc::default();
+    let mut wait_vec: Vec<JoinHandle<()>> = Vec::new();
+
+    for _ in 0..num_threads / 2 {
+        let map_clone = map.clone();
+        wait_vec.push(thread::spawn(move || {
+            for i in 0..10000 / num_threads{
+                map_clone.insert(i, i);
+            }
+            for i in 0..1000 / num_threads{
+                map_clone.get(&i);
+            }
+            for i in 0..700 / num_threads{
+                map_clone.get(&(i % 1000));
+            }
+            for i in 0..200 / num_threads{
+                map_clone.remove(&i);
+            }
+            for i in 200..400 / num_threads{
+                map_clone.insert(i, i + 1);
+            }
+        }));
+    }
+
+    for _ in 0..num_threads / 2 {
+        let map_clone = map.clone();
+        wait_vec.push(thread::spawn(move || {
+            for i in 10000..20000 / num_threads{
+                map_clone.insert(i, i);
+            }
+            for i in 10000..10200 / num_threads{
+                map_clone.get(&i);
+            }
+            for i in 0..700 / num_threads{
+                map_clone.get(&((i % 1000) + 1000));
+            }
+            for i in 10000..10200 / num_threads{
+                map_clone.remove(&i);
+            }
+            for i in 10200..10400 / num_threads{
+                map_clone.insert(i, i + 1);
+            }
+        }));
+    }
+
+    for handle in wait_vec {
+        handle.join().unwrap();
+    }
+}
+
 fn bench_equal_focus_lock_all(c: &mut Criterion) {
     c.bench_function_over_inputs("map_equal", |b: &mut Bencher, num_threads: &usize| b.iter(|| bench_equal_focus_lock(*num_threads)), 
                                 (2..42).filter(|num| num % 2 == 0).collect::<Vec<usize>>());
@@ -397,6 +572,25 @@ fn bench_heavy_insert_all(c: &mut Criterion) {
     (2..42).filter(|num| num % 2 == 0).collect::<Vec<usize>>());
 }
 
-criterion_group!(benches, bench_typical_lock_all, bench_typical_all, bench_equal_focus_lock_all, bench_equal_focus_all
-                        , bench_update_lock_all, bench_update_all, bench_heavy_insert_lock_all, bench_heavy_insert_all);
+fn chashmap_bench_equal(c: &mut Criterion) {
+    c.bench_function_over_inputs("chashmap_equal", |b: &mut Bencher, num_threads: &usize| b.iter(|| bench_equal_focus_chashmap(*num_threads)), 
+                                (2..42).filter(|num| num % 2 == 0).collect::<Vec<usize>>());
+}
+
+fn chashmap_bench_typical(c: &mut Criterion) {
+    c.bench_function_over_inputs("chashmap_typical", |b: &mut Bencher, num_threads: &usize| b.iter(|| bench_typical_chashmap(*num_threads)), 
+                                (2..42).filter(|num| num % 2 == 0).collect::<Vec<usize>>());
+}
+
+fn chashmap_bench_update(c: &mut Criterion) {
+    c.bench_function_over_inputs("chashmap_updates", |b: &mut Bencher, num_threads: &usize| b.iter(|| bench_with_updates_chashmap(*num_threads)), 
+                                (2..42).filter(|num| num % 2 == 0).collect::<Vec<usize>>());
+}
+
+fn chashmap_bench_insert(c: &mut Criterion) {
+    c.bench_function_over_inputs("chashmap_insert", |b: &mut Bencher, num_threads: &usize| b.iter(|| bench_heavy_insert_chashmap(*num_threads)), 
+                                (2..42).filter(|num| num % 2 == 0).collect::<Vec<usize>>());
+}
+
+criterion_group!(benches, chashmap_bench_equal, chashmap_bench_typical, chashmap_bench_update, chashmap_bench_insert);
 criterion_main!(benches);
--- benches/bench_stack.rs
+++ benches/bench_stack.rs
@@ -1,10 +1,11 @@
 #[macro_use]
 extern crate criterion;
 extern crate rustcurrent;
+extern crate crossbeam;
 
 use criterion::{Bencher, Criterion};
 use rustcurrent::structures::Stack;
-
+use crossbeam::sync::TreiberStack;
 use std::thread;
 use std::sync::{Arc, Mutex};
 use std::thread::JoinHandle;
@@ -13,7 +14,36 @@ fn bench_rustcurrent_stack(num_threads: usize, elim: bool) {
     let stack = Arc::new(Stack::new_with_collision_size(elim, num_threads / 2));
     let mut wait_vec: Vec<JoinHandle<()>> = Vec::new();
 
-    for _ in 0..num_threads * 2 {
+    for _ in 0..num_threads {
+        let mut s = stack.clone();
+        wait_vec.push(thread::spawn(move || {
+            for n in 0..10000 / num_threads {
+                s.push(n);
+            }         
+        }));
+        s = stack.clone();
+        wait_vec.push(thread::spawn(move || {
+            for n in 0..10000 / num_threads {
+                loop {
+                    match s.pop() {
+                        Some(v) => {break;}
+                        None => {} 
+                    }
+                }
+            }
+        }));
+    }
+
+    for handle in wait_vec {
+        handle.join().unwrap();
+    }
+}
+
+fn bench_crossbeam_stack(num_threads: usize, elim: bool) {
+    let stack = Arc::new(TreiberStack::new());
+    let mut wait_vec: Vec<JoinHandle<()>> = Vec::new();
+
+    for _ in 0..num_threads {
         let mut s = stack.clone();
         wait_vec.push(thread::spawn(move || {
             for n in 0..10000 / num_threads {
@@ -42,7 +72,7 @@ fn bench_locked_stack(num_threads: usize) {
     let stack: Arc<Mutex<Vec<usize>>> = Arc::new(Mutex::new(Vec::new()));
     let mut wait_vec: Vec<JoinHandle<()>> = Vec::new();
 
-    for _ in 0..num_threads * 2 {
+    for _ in 0..num_threads {
         let mut s = stack.clone();
         wait_vec.push(thread::spawn(move || {
             for n in 0..10000 / num_threads {
@@ -69,6 +99,39 @@ fn bench_locked_stack(num_threads: usize) {
 
 fn bench_mp_sc(num_threads: usize, elim: bool) {
     let stack = Arc::new(Stack::new_with_collision_size(elim, num_threads / 2));
+    let mut wait_vec = Vec::new();
+    
+    let amount = 10000 / num_threads;
+    let consumer_num = amount * (num_threads - 1);
+
+    let mut s = stack.clone();
+    wait_vec.push(thread::spawn(move || {
+        for i in 0..consumer_num {
+            loop {
+                match s.pop() {
+                    Some(v) => break,
+                    None => {}
+                }
+            }
+        }
+    }));
+
+    for _ in 0..(num_threads - 1) {
+        s = stack.clone();
+        wait_vec.push(thread::spawn(move || {
+            for i in 0..amount {
+                s.push(i);
+            }
+        }))
+    }
+
+    for handle in wait_vec {
+        handle.join().unwrap();
+    }
+}
+
+fn bench_mp_sc_crossbeam(num_threads: usize, elim: bool) {
+    let stack = Arc::new(TreiberStack::new());
     let mut wait_vec = Vec::new();
     
     let amount = 10000 / num_threads;
@@ -143,7 +206,7 @@ fn bench_sp_mc(num_threads: usize, elim: bool) {
     for _ in 0..(num_threads - 1) {
         let s = stack.clone();
         wait_vec.push(thread::spawn(move || {
-            for i in 0..producer_num {
+            for i in 0..amount {
                 s.push(i);
             }
         }));
@@ -151,7 +214,40 @@ fn bench_sp_mc(num_threads: usize, elim: bool) {
     
     let s = stack.clone();
     wait_vec.push(thread::spawn(move || {
-        for i in 0..amount {
+        for i in 0..producer_num {
+            loop {
+                match s.pop() {
+                    Some(v) => break,
+                    None => {}
+                }
+            }
+        }
+    }));
+
+    for handle in wait_vec {
+        handle.join().unwrap();
+    }
+}
+
+fn bench_sp_mc_crossbeam(num_threads: usize, elim: bool) {
+    let stack = Arc::new(Stack::new_with_collision_size(elim, num_threads / 2));
+    let mut wait_vec = Vec::new();
+    
+    let amount = 10000 / num_threads;
+    let producer_num = amount * (num_threads - 1);
+
+    for _ in 0..(num_threads - 1) {
+        let s = stack.clone();
+        wait_vec.push(thread::spawn(move || {
+            for i in 0..amount {
+                s.push(i);
+            }
+        }));
+    }
+    
+    let s = stack.clone();
+    wait_vec.push(thread::spawn(move || {
+        for i in 0..producer_num {
             loop {
                 match s.pop() {
                     Some(v) => break,
@@ -176,7 +272,7 @@ fn bench_sp_mc_lock(num_threads: usize) {
     for _ in 0..(num_threads - 1) {
         let s = stack.clone();
         wait_vec.push(thread::spawn(move || {
-            for i in 0..producer_num {
+            for i in 0..amount {
                 s.lock().unwrap().push(i);
             }
         }));
@@ -184,7 +280,7 @@ fn bench_sp_mc_lock(num_threads: usize) {
     
     let s = stack.clone();
     wait_vec.push(thread::spawn(move || {
-        for i in 0..amount {
+        for i in 0..producer_num {
             loop {
                 match s.lock().unwrap().pop() {
                     Some(v) => break,
@@ -235,5 +331,17 @@ fn bench_no_elim_sp_mc(c: &mut Criterion) {
     c.bench_function_over_inputs("stack_sp_mc_no_elim", |b: &mut Bencher, num_threads: &usize| b.iter(|| bench_sp_mc(*num_threads, false)), (2..42).filter(|num| num % 2 == 0).collect::<Vec<usize>>());
 }
 
-criterion_group!(benches, bench_no_elim_mp_sc, bench_no_elim_sp_mc);
+fn bench_crossbeam_equal(c: &mut Criterion) {
+    c.bench_function_over_inputs("cross_stack_equal", |b: &mut Bencher, num_threads: &usize| b.iter(|| bench_crossbeam_stack(*num_threads, true)), (2..42).filter(|num| num % 2 == 0).collect::<Vec<usize>>());
+}
+
+fn bench_crossbeam_mp_sc(c: &mut Criterion) {
+    c.bench_function_over_inputs("cross_stack_mp_sc", |b: &mut Bencher, num_threads: &usize| b.iter(|| bench_mp_sc_crossbeam(*num_threads, true)), (2..42).filter(|num| num % 2 == 0).collect::<Vec<usize>>());
+}
+
+fn bench_crossbeam_sp_mc(c: &mut Criterion) {
+    c.bench_function_over_inputs("cross_stack_sp_mc", |b: &mut Bencher, num_threads: &usize| b.iter(|| bench_sp_mc_crossbeam(*num_threads, true)), (2..42).filter(|num| num % 2 == 0).collect::<Vec<usize>>());
+}
+
+criterion_group!(benches, bench_crossbeam_mp_sc, bench_crossbeam_sp_mc);
 criterion_main!(benches);
